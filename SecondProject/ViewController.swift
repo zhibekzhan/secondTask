e9adc8a7fc08f9cc1272dc6bc66b14c37499e772
Kyzzhibek Almaz kyzy <kyzzhibek0908@gmail.com>
2023-04-12 16:26:17 +0600

    Initial Commit

---
+++ SecondProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SecondProject
+//
+//  Created by mac-user on 12/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
